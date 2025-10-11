61640550d2a9ecdbb2e5ab564e553168e9d06f3a
roman krasilnikov <krasilnikovroman5@gmail.com>
2025-10-11 13:32:47 +0700

    tracks search

--- src/app/api/track/list/route.ts
+++ src/app/api/track/list/route.ts
@@ -1,66 +1,68 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { TrackType } from '../../../../../@types/track'
 import { db } from '@/db'
-import { PlaylistTable, PlaylistTrackTable, TrackTable } from '@/db/schema'
-import { desc, eq, sql } from 'drizzle-orm'
+import { sql } from 'drizzle-orm'
 import getUserBySession from '@/authentification/actions/getUserBySession'
 
 export async function GET(req: NextRequest) {
     try {
         const { searchParams } = new URL(req.url)
-        console.log(searchParams)
-        const search = Number(searchParams.get('search') || '')
+
+        const search = searchParams.get('q')?.trim() || ''
         const offset = Number(searchParams.get('offset') || '0')
         const limit = Number(searchParams.get('limit') || '20')
+
         const user = await getUserBySession()
         const userId = user?.id ?? null
 
-        const data = await db
-            .select({
-                id: TrackTable.id,
-                name: TrackTable.name,
-                author: TrackTable.author,
-                imageName: TrackTable.imageName,
-                createdAt: TrackTable.createdAt,
-                duration: TrackTable.duration,
-                trackDir: TrackTable.trackDir,
-                likesCount: sql<number>`
-                            COUNT(
+        const query = sql`
+            SELECT 
+                t.id,
+                t.name,
+                t.author,
+                t."imageName",
+                t."createdAt",
+                t.duration,
+                t."trackDir",
+                COUNT(
+                    CASE 
+                        WHEN p.type = 'default' 
+                        THEN pt."trackId"
+                    END
+                ) AS "likesCount",
+                ${
+                    userId
+                        ? sql`
+                            BOOL_OR(
                                 CASE
-                                    WHEN ${PlaylistTable.type} = 'default'
-                                    THEN ${PlaylistTrackTable.trackId}
+                                    WHEN p.type = 'default' AND p."creatorId" = ${userId}
+                                    THEN TRUE
+                                    ELSE FALSE
                                 END
-                            )
-                        `,
-                isLikedByCurrentUser: userId
-                    ? sql<boolean>`
-                        BOOL_OR(
-                            CASE 
-                                WHEN ${PlaylistTable.type} = 'default'
-                                  AND ${PlaylistTable.creatorId} = ${userId}
-                                THEN TRUE
-                                ELSE FALSE
-                            END
-                        )
-                    `
-                    : sql<boolean>`FALSE`,
-            })
-            .from(TrackTable)
-            .leftJoin(
-                PlaylistTrackTable,
-                eq(TrackTable.id, PlaylistTrackTable.trackId),
-            )
-            .leftJoin(
-                PlaylistTable,
-                eq(PlaylistTable.id, PlaylistTrackTable.playlistId),
-            )
-            .groupBy(TrackTable.id)
-            .orderBy(desc(TrackTable.createdAt))
-            .limit(limit)
-            .offset(offset)
+                            ) AS "isLikedByCurrentUser"`
+                        : sql`FALSE AS "isLikedByCurrentUser"`
+                }
+            FROM track_table t
+            LEFT JOIN playlist_tracks_table pt ON t.id = pt."trackId"
+            LEFT JOIN playlist_table p ON p.id = pt."playlistId"
+            ${
+                search
+                    ? sql`WHERE t.name ILIKE ${'%' + search + '%'} OR t.author ILIKE ${'%' + search + '%'}`
+                    : sql``
+            }
+            GROUP BY t.id
+            ORDER BY t."createdAt" DESC
+            LIMIT ${limit}
+            OFFSET ${offset};
+        `
 
-        return NextResponse.json(data as TrackType[])
+        const result = await db.execute(query)
+
+        return NextResponse.json(result)
     } catch (error) {
-        console.error(error)
+        console.error('GET /tracks error:', error)
+        return NextResponse.json(
+            { error: 'Internal Server Error' },
+            { status: 500 },
+        )
     }
 }
