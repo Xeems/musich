ee5dc06d16106468892db77d3002969a4e0a5bb5
roman krasilnikov <krasilnikovroman5@gmail.com>
2025-12-22 22:17:50 +0700

    track/[id] api route

--- src/app/api/track/[id]/route.ts
+++ src/app/api/track/[id]/route.ts
@@ -1,12 +1,29 @@
+import { db } from '@/db'
 import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET(request: NextRequest) {
+export async function GET(
+    request: NextRequest,
+    { params }: { params: Promise<{ id: string }> },
+) {
+    const { id } = await params
     try {
-        return new NextResponse(JSON.stringify({ success: true }), {
-            status: 200,
-            headers: { 'Content-Type': 'application/json' },
+        const track = await db.query.TrackTable.findFirst({
+            where: (tracks, { eq }) => eq(tracks.id, id),
         })
+        if (!track) return NextResponse.json({}, { status: 404 })
+
+        return NextResponse.json(
+            { data: track },
+            {
+                status: 200,
+                headers: { 'Content-Type': 'application/json' },
+            },
+        )
     } catch (e) {
         console.error(e)
+        return NextResponse.json(
+            { error: 'Internal server error' },
+            { status: 500 },
+        )
     }
 }
